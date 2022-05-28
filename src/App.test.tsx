75355f72260027466666d410afefb852a20f8789
kooku <kooku@kakao.com>
2022-05-28 21:31:08 +0900

    env: test 환경 셋팅

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  const renderApp = () => render(<App />);
+
+  context('rendering', () => {
+    it('hello가 보여야 한다.', ()=> {
+      const { container } = renderApp();
+      
+      expect(container).toHaveTextContent('hello');
+    })
+  })
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div>
-      <header>
-        hello
-      </header>
+      hello
     </div>
   );
 }
