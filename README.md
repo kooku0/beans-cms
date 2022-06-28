74a4eb8866c78a379c6807eb147f3a95c2f8bd8e
kooku <kooku@kakao.com>
2022-06-29 00:59:29 +0900

    feat: update favicon & readme

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
     <a href="https://codecov.io/gh/kooku0/headless-cms">
       <img src="https://codecov.io/gh/kooku0/headless-cms/branch/main/graph/badge.svg?token=CWOMDLZBC4"/>
     </a>
+     <a href="https://vercel.com/kooku0/beans-cms/deployments">
+      <img src="http://therealsujitk-vercel-badge.vercel.app/?app=beans-cms"/>
+    </a>
   </p>
 </div>
 
---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from 'react';
+
+import Document, {
+  DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
