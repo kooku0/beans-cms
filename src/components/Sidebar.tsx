879abe13f0f4ca0759474e790ea8568184e903dc
kooku <koomg9599@gmail.com>
2022-06-30 04:22:37 +0900

    feat: create sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,52 @@
+import {
+  createContext, Dispatch, PropsWithChildren, SetStateAction, useContext, useMemo, useState,
+} from 'react';
+
+import { css } from '@emotion/react';
+
+type TSideBarContext = {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const SidebarContext = createContext<TSideBarContext | null>(null);
+const useSidebarContext = () => useContext(SidebarContext);
+
+export function Sidebar({ children }: PropsWithChildren) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const value = useMemo(() => ({ isOpen, setIsOpen }), []);
+
+  return (
+    <SidebarContext.Provider value={value}>
+      <div css={css`
+        width: 225px;
+        height: 100vh;
+        padding: 20px 0;
+        background-color: skyblue;
+      `}
+      >
+        {children}
+      </div>
+    </SidebarContext.Provider>
+  );
+}
+
+interface MenuProps {
+  link: string;
+  label: string;
+}
+
+export function Menu({ children, link, label }: PropsWithChildren<MenuProps>) {
+  const { isOpen } = useSidebarContext() as TSideBarContext;
+
+  const handleClickMenu = () => window.open(link);
+
+  return (
+    // eslint-disable-next-line jsx-a11y/interactive-supports-focus
+    <div onClick={handleClickMenu} role="button">
+      <div>{label}</div>
+      <div>{children}</div>
+    </div>
+  );
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,24 @@
+import { css } from '@emotion/react';
 import { Button } from '@nextui-org/react';
+
+import { Menu, Sidebar } from '@/components/Sidebar';
 
 function HomePage() {
   return (
-    <div>
-      <div>
-        Home
-      </div>
-      <Button>버튼</Button>
+    <div css={css`display: flex;`}>
+      <Sidebar>
+        <img src="/logo.png" alt="logo" />
+        <Menu label="Contents" link="/contents" />
+        <Menu label="Images" link="/images" />
+        <Menu label="Authors" link="/authors" />
+        <Menu label="Tags" link="/tags" />
+      </Sidebar>
+      <main>
+        <div>
+          Home
+        </div>
+        <Button>버튼</Button>
+      </main>
     </div>
   );
 }
