83dc3a2eb28fb5c59173a3c13284e423289c331b
kooku <koomg9599@gmail.com>
2022-07-01 09:51:02 +0900

    feat: create sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,52 +1,139 @@
 import {
-  createContext, Dispatch, PropsWithChildren, SetStateAction, useContext, useMemo, useState,
+  PropsWithChildren, ReactNode, useEffect, useRef, useState,
 } from 'react';
+import { a, useSpring } from 'react-spring';
+import { useMeasure } from 'react-use';
 
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
-type TSideBarContext = {
-  isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
-};
+import ChevronDown from '@/assets/chevron-down.svg';
 
-const SidebarContext = createContext<TSideBarContext | null>(null);
-const useSidebarContext = () => useContext(SidebarContext);
+function usePrevious<T>(value: T) {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+}
 
-export function Sidebar({ children }: PropsWithChildren) {
-  const [isOpen, setIsOpen] = useState(false);
+type MenuConfig = {
+  icon?: string;
+  text: string;
+  link: string;
+  items?: SubMenuConfig[];
+}
 
-  const value = useMemo(() => ({ isOpen, setIsOpen }), []);
+type SubMenuConfig = Omit<MenuConfig, 'items'>;
 
+interface SidebarProps {
+  config: MenuConfig[];
+  header?: ReactNode;
+  footer?: ReactNode;
+}
+
+function Sidebar({
+  config, header, footer,
+}: PropsWithChildren<SidebarProps>) {
   return (
-    <SidebarContext.Provider value={value}>
-      <div css={css`
+    <div css={css`
         width: 225px;
         height: 100vh;
         padding: 20px 0;
         background-color: skyblue;
+        position: relative;
       `}
-      >
-        {children}
-      </div>
-    </SidebarContext.Provider>
+    >
+      {header}
+      {
+        config.map((menu) => (
+          <Menu
+            key={menu.link}
+            text={menu.text}
+            link={menu.link}
+            items={menu.items}
+          />
+        ))
+      }
+      {footer}
+    </div>
   );
 }
 
 interface MenuProps {
   link: string;
-  label: string;
+  text: string;
+  items?: SubMenuConfig[];
 }
 
-export function Menu({ children, link, label }: PropsWithChildren<MenuProps>) {
-  const { isOpen } = useSidebarContext() as TSideBarContext;
+function Menu({ link, text, items }: PropsWithChildren<MenuProps>) {
+  const [isOpen, setOpen] = useState(false);
+  const previous = usePrevious(isOpen);
+  const [ref, { height: viewHeight }] = useMeasure<HTMLDivElement>();
 
-  const handleClickMenu = () => window.open(link);
+  const { height, opacity, y } = useSpring({
+    from: { height: 0, opacity: 0, y: 0 },
+    to: {
+      height: isOpen ? viewHeight : 0,
+      opacity: isOpen ? 1 : 0,
+      y: isOpen ? 0 : 20,
+    },
+  });
+
+  const handleClick = () => setOpen(!isOpen);
 
   return (
-    // eslint-disable-next-line jsx-a11y/interactive-supports-focus
-    <div onClick={handleClickMenu} role="button">
-      <div>{label}</div>
-      <div>{children}</div>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      onClick={handleClick}
+      css={css`
+        border-bottom: solid 1px black;
+    `}
+    >
+      <div css={css`
+        padding: 16px 8px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      `}
+      >
+        {text}
+        {items && <ChevronDown />}
+      </div>
+      <a.div
+        style={{
+          opacity,
+          height: isOpen && previous === isOpen ? 'auto' : height,
+          overflow: 'hidden',
+        }}
+      >
+        <div ref={ref}>
+          {
+            items?.map((item) => (
+              <SubMenu key={item.link}>
+                {item.text}
+              </SubMenu>
+            ))
+          }
+        </div>
+      </a.div>
     </div>
   );
 }
+
+const SubMenu = styled.div`
+  padding: 13px 30px;
+  padding-left: 16px;
+  background-color: whitesmoke;
+`;
+const Header = styled.div``;
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
+
+Sidebar.Header = Header;
+Sidebar.Footer = Footer;
+
+export default Sidebar;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,18 +1,51 @@
 import { css } from '@emotion/react';
 import { Button } from '@nextui-org/react';
 
-import { Menu, Sidebar } from '@/components/Sidebar';
+import Sidebar from '@/components/Sidebar';
 
 function HomePage() {
+  const sidebarConfig = [
+    {
+      text: 'Contents',
+      link: '/contents',
+      items: [
+        {
+          text: 'Authors',
+          link: '/meta/authors',
+        },
+      ],
+    },
+    {
+      text: 'Meta',
+      link: '/meta',
+      items: [
+        {
+          text: 'Authors',
+          link: '/meta/authors',
+        },
+      ],
+    },
+    {
+      text: 'Images',
+      link: '/images',
+    },
+  ];
+
   return (
     <div css={css`display: flex;`}>
-      <Sidebar>
-        <img src="/logo.png" alt="logo" />
-        <Menu label="Contents" link="/contents" />
-        <Menu label="Images" link="/images" />
-        <Menu label="Authors" link="/authors" />
-        <Menu label="Tags" link="/tags" />
-      </Sidebar>
+      <Sidebar
+        config={sidebarConfig}
+        header={(
+          <Sidebar.Header>
+            <img src="/logo.png" alt="logo" />
+          </Sidebar.Header>
+        )}
+        footer={(
+          <Sidebar.Footer>
+            footer
+          </Sidebar.Footer>
+        )}
+      />
       <main>
         <div>
           Home
