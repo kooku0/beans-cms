9808bc341f8879326d70f7a5e2e4a9b249ca1d40
kooku <koomg9599@gmail.com>
2022-07-12 09:27:31 +0900

    feat: update create button handler

--- src/pages/authors/index.page.tsx
+++ src/pages/authors/index.page.tsx
@@ -1,16 +1,21 @@
 import { mdiPlus } from '@mdi/js';
 import Icon from '@mdi/react';
 import { Button } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 import Authors from '@/components/author/Authors';
 import Layout from '@/components/common/Layout';
 
 function AuthorsPage() {
+  const router = useRouter();
+
+  const handleCreate = () => router.push('/authors/create');
+
   return (
     <Layout
       title="Authors"
       right={(
-        <Button data-testid="create-button" auto icon={<Icon path={mdiPlus} size={1} />} href="/authors/create">
+        <Button auto icon={<Icon path={mdiPlus} size={1} />} onPress={handleCreate}>
           Create
         </Button>
       )}
--- src/pages/posts/index.page.tsx
+++ src/pages/posts/index.page.tsx
@@ -1,16 +1,21 @@
 import { mdiPlus } from '@mdi/js';
 import Icon from '@mdi/react';
 import { Button } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 import Layout from '@/components/common/Layout';
 import Posts from '@/components/post/Posts';
 
 function PostsPage() {
+  const router = useRouter();
+
+  const handleCreate = () => router.push('/posts/create');
+
   return (
     <Layout
       title="Posts"
       right={(
-        <Button data-testid="create-button" auto icon={<Icon path={mdiPlus} size={1} />} href="/posts/create">
+        <Button auto icon={<Icon path={mdiPlus} size={1} />} onPress={handleCreate}>
           Create
         </Button>
       )}
