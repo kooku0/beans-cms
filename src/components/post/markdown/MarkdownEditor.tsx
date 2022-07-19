63225bd0c9b717972d77122389bda41654a3ab16
kooku <kooku@kakao.com>
2022-07-20 02:18:33 +0900

    feat: apply inline grammar (#24)

--- src/components/post/markdown/MarkdownEditor.tsx
+++ src/components/post/markdown/MarkdownEditor.tsx
@@ -21,34 +21,11 @@ function MarkdownEditor({ markdown, setMarkdown }: Props) {
   const handleClickToolbarItem = (grammar: MarkdownGrammar) => {
     const cursorPosition = textAreaRef.current!.selectionStart;
 
-    const newLineGrammars = ['h1', 'h2', 'h3', 'h4'];
-    const rangeGrammars = ['bold', 'italic', 'underline', 'strikethrough'];
-    const inlineGrammars = ['link', 'image'];
-
     const [textBeforeCursor, textAfterCursor] = slicedTextBeforeAfterCursor(
-      newLineGrammars.includes(grammar), cursorPosition,
+      grammar, cursorPosition,
     );
 
     applyMarkdownGrammar(grammar, textBeforeCursor, textAfterCursor);
-  };
-
-  const slicedTextBeforeAfterCursor = (
-    isNewLineGrammar: boolean,
-    cursorPosition: number) => {
-    if (isNewLineGrammar) {
-      const sliced = markdown.slice(0, cursorPosition);
-      const lastNewLineIndex = sliced.lastIndexOf('\n');
-
-      const textBeforeCursor = sliced.slice(0, lastNewLineIndex + 1);
-      const textAfterCursor = markdown.slice(lastNewLineIndex + 1, markdown.length);
-
-      return [textBeforeCursor, textAfterCursor];
-    }
-
-    const textBeforeCursor = markdown.slice(0, cursorPosition);
-    const textAfterCursor = markdown.slice(cursorPosition, markdown.length);
-
-    return [textBeforeCursor, textAfterCursor];
   };
 
   const applyMarkdownGrammar = (grammar: MarkdownGrammar, before: string, after: string) => {
@@ -65,9 +42,40 @@ function MarkdownEditor({ markdown, setMarkdown }: Props) {
       case 'h4':
         setMarkdown(`${before}#### ${after}`);
         break;
+      case 'link':
+        setMarkdown(`${before}[link text]()${after}`);
+        break;
+      case 'image':
+        setMarkdown(`${before}![image]()${after}`);
+        break;
       default:
         break;
     }
+  };
+
+  const slicedTextBeforeAfterCursor = (grammar: MarkdownGrammar, cursorPosition: number) => {
+    const newLineGrammars = ['h1', 'h2', 'h3', 'h4'];
+    const rangeGrammars = ['bold', 'italic', 'underline', 'strikethrough'];
+    const inlineGrammars = ['link', 'image'];
+
+    if (newLineGrammars.includes(grammar)) {
+      const sliced = markdown.slice(0, cursorPosition);
+      const lastNewLineIndex = sliced.lastIndexOf('\n');
+
+      const textBeforeCursor = sliced.slice(0, lastNewLineIndex + 1);
+      const textAfterCursor = markdown.slice(lastNewLineIndex + 1, markdown.length);
+
+      return [textBeforeCursor, textAfterCursor];
+    }
+
+    if (inlineGrammars.includes(grammar)) {
+      const textBeforeCursor = markdown.slice(0, cursorPosition);
+      const textAfterCursor = markdown.slice(cursorPosition, markdown.length);
+
+      return [textBeforeCursor, textAfterCursor];
+    }
+
+    return ['', ''];
   };
 
   return (
